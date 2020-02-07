b13f9ef624106147c82e33db4284ff53e7b22843
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-02-07 10:26:05 +0100

    question 2

--- exercise_1/js/script.js
+++ exercise_1/js/script.js
@@ -7,6 +7,11 @@ There is a notification banner on the site
 
 1. add a friendly message to the notification "notification-content"
 2. add a class to make the banner green, use this class: has-background-primary from: https://bulma.io/documentation/modifiers/color-helpers/
-3. you will need to create an event listener and a function to do the above work
 
+*/
+
+// 2::::
+/*
+ The notification banner should not always show.
+ 1. only show the notification banner when you click the login button. (clue, this is an event.)
 */
