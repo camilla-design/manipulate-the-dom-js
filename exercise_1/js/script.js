9e803f3635e2a225048f5ee20307e2b001a1c672
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-02-07 12:10:50 +0100

    update, in progress

--- exercise_1/js/script.js
+++ exercise_1/js/script.js
@@ -8,35 +8,26 @@ There is a notification banner on the site
 1. add a friendly message to the notification "notification-content"
 2. add a class to make the banner green, use this class: has-background-primary from: https://bulma.io/documentation/modifiers/color-helpers/
 
-// 2:::
-only show the notification banner when you click the login button, (this is an event.)
-
-// 3:::
-add a photo as a background image to hero banner
-*/
-
-
 let notification = document.querySelector(".has-background-primary");
 notification.innerHTML = "Hello! Welcome to my page";
 
 let changeBackgroundColor = document.querySelector(".notification");
 changeBackgroundColor.style.backgroundColor = "has-background-primary";
 
-const showBanner = document.querySelector(".button is-light");
+// 2:::
+only show the notification banner when you click the login button, (this is an event.)
+
+
+let logInButton = document.querySelector(".is-light");
 notification.style.display = "none";
 
 
-notification.addEventListener("click", showBanner);
-// 2::::
-/*
- The notification banner should not always show.
- 1. only show the notification banner when you click the login button. (clue, this is an event.)
+logInButton.addEventListener("click", notification);
+
+// 3:::
+add a photo as a background image to hero banner
 */
 
-// 3::::
-/*
-  The hero banner looks sad without an image, please add a nice photo as background image to the hero banner.
-*/
 
 // 4::::
 /*
