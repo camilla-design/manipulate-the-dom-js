fac6b8437fe990b0f9cb3904c1e2b4488451d1e7
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-02-07 10:17:44 +0100

    instructions

--- exercise_1/js/script.js
+++ exercise_1/js/script.js
@@ -1,19 +1,12 @@
-const containerDiv = document.querySelector(".container");
-const notificationBanner = document.querySelector(".notification");
-
-let notificationContent = ``;
-
-let warningMessage = () => {
-    notificationContent = `Warning you have entered incorrect email`;
-    
-    
-    containerDiv.innerHTML = notificationContent;
-}
-
-containerDiv.addEventListener("click", warningMessage);
-
-
 let currentDate = document.querySelector("[data-date='currentDate']");
-
 currentDate.innerHTML = new Date();
 
+// 1::::
+/*
+There is a notification banner on the site
+
+1. add a friendly message to the notification "notification-content"
+2. add a class to make the banner green, use this class: has-background-primary from: https://bulma.io/documentation/modifiers/color-helpers/
+3. you will need to create an event listener and a function to do the above work
+
+*/
